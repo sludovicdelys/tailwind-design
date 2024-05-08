77829ab91bab337157fe0cb5377d649cc9eb5e1f
Sabrina Ludovic de Lys <ludovicdelyssabrina@gmail.com>
2024-05-08 17:44:34 +0200

    tutorial : customizing fonts

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    spacing: {
+      '1': '100px',
+      '2': '12px',
+      '3': '16px',
+      '4': '24px',
+      '5': '32px',
+      '6': '48px',
+    },
+    extend: {
+      fontFamily: {
+        sans: '"Alegreya Sans", sans-serif',
+        'poe': '"Poetsen One", sans-serif',
+      },
+      fontSize: {
+        xs: '12px',
+        sm: '15px',
+        base: '18px',
+      },
+      colors: {
+        'vuejs': '#48e669',
+      },
+    },
+  },
+  plugins: [],
+}
+
